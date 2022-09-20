c259a90e01e20f8bf40fe26c4dd81012a49c19c1
Agostina <lozagostina@gmail.com>
2022-09-19 22:03:47 -0300

    agrego ejercicio 1 y 2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Titulo from "./components/Titulo";
+
+function App() {
+  const persona = 'My friend';
+
+  return (
+ <Titulo contenidoPersona={persona} estado={true}/>
+  );
+}
+
+export default App;
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Titulo = (props) => {
+    return (
+        <h1>Hello {props.contenidoPersona}</h1>
+    );
+};
+
+export default Titulo;
